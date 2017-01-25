74a4bbf5abba502a588b8c2695291fd92fd4ef7b
Anqi Xu <axu@ucvts.org>
2017-01-25 21:26:43 +0000

    edited stuff

--- idea-one/visual.py
+++ idea-one/visual.py
@@ -1,6 +1,13 @@
 import pandas as pd
-import numpy as np
-from bokeh.charts import Scatter, output_file, save
+import requests
+import csv
+import datetime
+import math
+from bokeh.plotting import figure, output_file, save
+from bokeh.models import HoverTool
+import life_expectancy
+
+life_expectancy.an_approximation_of_how_long_you_have_until_you_die()
 
 df = pd.read_csv('uglylifechart.csv')   # read the data from the lifechart file
 
@@ -14,12 +21,27 @@ current_age = []
 for c in df['age']:     # for each 'item' in the column 'age'
     current_age.append(c)       # add 'item' to the list 'current_age'
 
-p = Scatter(df, x='age', y='life remaining', xlabel='current age', ylabel='life remaining', title='determine how long you have left to live')
+        
+# hover
+hover = HoverTool(
+        tooltips=[
+            ('age', '@age')
+            ('remaining', '@lifs')
+        ]
+    )
+
+
+p = figure(plot_width=1500, plot_height=900, x_axis_label='current age', y_axis_label='life remaining', tools=[hover])
+
+# create the circles
+for x in range(2, 162):
+    if x % 2 == 0:
+        p.circle(current_age[x], life_remaining[x], size=10, color='#404387', alpha=0.5)
+    else:
+        p.circle(current_age[x], life_remaining[x], size=10, color='#22A784', alpha=0.5)
+
 
 # file
 output_file('life.html')
-
-# create the circles
-p.circle(current_age, life_remaining, size=10, color='lavender', alpha=0.5)
 
 save(p)
